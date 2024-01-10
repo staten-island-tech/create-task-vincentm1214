0cd01f929d008d3dfb3799332ad1cda84c1746dd
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-01-10 10:14:43 -0500

    1

---
+++ create-project/js/arr.js
@@ -0,0 +1,13 @@
+const items=[
+    {
+        name:"square",
+        sides:"four",
+        equalsides:true,
+        equalangles:true,
+        smelly:false,
+        DOB:2007,
+        spouse:null,
+        siblings:["tink", "tonk"],
+        },
+        
+]
